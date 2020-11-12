4cac1de1c5712840eeb7b462302ce20a69ede427
ThiagoSSales <thiago.ss133@gmail.com>
2020-11-12 07:01:07 -0300

    Estrutura

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Nav, NavbarContainer, NavLogo} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to="/">
+            dollar
+          </NavLogo>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
